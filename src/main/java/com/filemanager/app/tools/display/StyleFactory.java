6ac9bc94f4aa1dcba0b8f318dc9df5b275af0fc2
hrcao <chrse1997@163.com>
2026-01-12 00:29:28 +0800

    添加license信息

--- src/main/java/com/filemanager/app/tools/display/StyleFactory.java
+++ src/main/java/com/filemanager/app/tools/display/StyleFactory.java
@@ -1,3 +1,12 @@
+/* 
+ * Copyright (c) 2026 hrcao (chrse1997@163.com) 
+ * Licensed under GPLv3 + Non-Commercial Clause. 
+ * You may not use this file except in compliance with the License. 
+ * See the LICENSE file in the project root for more information. 
+ * Author: hrcao 
+ * Mail: chrse1997@163.com 
+ * Date: 2026-01-12 
+ */
 package com.filemanager.app.tools.display;
 
 import com.filemanager.model.ChangeRecord;
